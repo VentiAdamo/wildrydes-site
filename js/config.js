804a6052fdec2b0bf9b226a483419bce77fdf1f0
VentiAdamo <56836457+VentiAdamo@users.noreply.github.com>
2025-03-15 17:23:28 -0400

    Update config.js
    second attempt at user pool changes

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_dXp1Zscky', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4mvpj6c8c3agpvv5kao6tq57ge', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_uOQfT4WZz', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2ocd9lgggh6omntetliip9kcjq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
